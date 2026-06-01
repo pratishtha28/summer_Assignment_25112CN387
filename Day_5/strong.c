10e7a95f061217e861d05f6668e87f321ffecb1f
pratishtha <srivastavanishi905@gmail.com>
2026-06-02 00:36:16 +0530

    day5_programs

--- Day_5/strong.c
+++ Day_5/strong.c
@@ -1,18 +1,24 @@
-#include<stdio.h>
-int main(){
-    int n, sum = 1;
+#include <stdio.h>
+int main()
+{
+    int n, temp, digit, sum = 0, fact, i;
     printf("Enter a number: ");
-    scanf("%d",&n);
-    for(int i=2;i<=n/2;i++){
-        if(n%i==0){
-            sum+=i;
+    scanf("%d", &n);
+    temp = n;
+    while(temp > 0)
+    {
+        digit = temp % 10;
+        fact = 1;
+        for(i = 1; i <= digit; i++)
+        {
+            fact = fact * i;
         }
+        sum = sum + fact;
+        temp = temp / 10;
     }
-    if(sum==n && n!=1){
-        printf("Strong Number");
-    }
-    else{
-        printf("Not a Strong Number");
-    }
+    if(sum == n)
+        printf("%d is a Strong Number", n);
+    else
+        printf("%d is not a Strong Number", n);
     return 0;
 }
